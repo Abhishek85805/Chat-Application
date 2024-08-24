9ff278c7c65379294fb7159e11e0af9b00600446
Abhishek85805 <abhishekchauhan85805@gmail.com>
2024-08-24 16:28:39 +0530

    change cors

--- backend/src/index.js
+++ backend/src/index.js
@@ -35,7 +35,7 @@ const server = http.createServer(app);
 const io = new SocketIo(server, {
     pingTimeout: 60000,
     cors: {
-      origin: "http://localhost:5173",
+      origin: ["http://localhost:5173", "https://chit-chat-abhishek.netlify.app"],
     },
 });
 
